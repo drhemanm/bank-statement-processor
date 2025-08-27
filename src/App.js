f7d98ad54e57da6adc394491273d6281a56273a4
drhemanm <heman.m@iafrica.solutions>
2025-08-27 11:16:17 +0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BankStatementProcessor from './BankStatementProcessor';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BankStatementProcessor />
+    </div>
+  );
+}
+
+export default App;
